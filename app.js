7774118f1455829316626a6b9a94a61c350ccf94
ahmedjoseph07 <ahmedjoseph11@gmail.com>
2024-07-01 01:01:28 +0600

    ejs view set up,index route

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require("mongoose")
 const Listing = require("./models/listing.js")
-
+const path = require("path");
 const MONGO_URL = "mongodb://127.0.0.1:27017/haven";
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 main().then(() => {
     console.log("Connected to DB");
@@ -21,18 +25,24 @@ app.get("/", (req, res) => {
     res.send("I am root");
 })
 
-app.get("/testListing", async(req, res) => {
-    let sampleListing = new Listing({
-        title: "My New villa",
-        description: "By the sea beach",
-        price: 1300,
-        location: "Chittagong",
-        country: "Bangladesh",
-    });
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("successful testing ");
+//Index Route
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
 })
+
+// app.get("/testListing", async(req, res) => {
+//     let sampleListing = new Listing({
+//         title: "My New villa",
+//         description: "By the sea beach",
+//         price: 1300,
+//         location: "Chittagong",
+//         country: "Bangladesh",
+//     });
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful testing ");
+// })
 
 
 app.listen(8080, () => {
