904a1b55eaa96e35164e3d08052e1d6b222b1dfd
ahmedjoseph07 <ahmedjoseph11@gmail.com>
2024-07-11 20:14:33 +0600

    schema validation using joi

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const path = require("path");
 const MONGO_URL = "mongodb://127.0.0.1:27017/haven";
 const methodOverride = require("method-override");
 const engine = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js")
-const ExpressError = require("./utils/ExpressError.js")
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 main().then(() => {
     console.log("Connected to DB");
@@ -32,6 +33,15 @@ app.get("/", (req, res) => {
     res.send("I am root");
 })
 
+const validateListing = (req,res,next) =>{
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((item)=>item.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+}
 //Index Route
 app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
@@ -52,10 +62,8 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
 }))
 
 // Create Route
-app.post("/listings", wrapAsync(async (req, res, next) => {
-    if(!req.body.listing){
-        throw new ExpressError(400,"Send valid data");
-    }
+app.post("/listings",validateListing, wrapAsync(async (req, res, next) => {
+    
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -70,10 +78,7 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
 }))
 
 // Update Route
-app.put("/listings/:id", wrapAsync(async (req, res) => {
-    if(!req.body.listing){
-        throw new ExpressError(400,"Send valid data");
-    }
+app.put("/listings/:id",validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
