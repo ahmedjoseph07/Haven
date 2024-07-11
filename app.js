52c00b3c6ffbfd86ba3204205ad1dc7a21236cc1
ahmedjoseph07 <ahmedjoseph11@gmail.com>
2024-07-11 18:36:00 +0600

    styled error page added

--- app.js
+++ app.js
@@ -93,7 +93,9 @@ app.all("*",(req,res,next)=>{
 
 app.use((err, req, res, next) => {
     let {statusCode=500,message="something went wrong"} = err;
-    res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs",{err});
+    // res.status(statusCode).send(message);
+
 })
 
 app.listen(8080, () => {
