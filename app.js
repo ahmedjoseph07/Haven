8f1e1617f7a9464d88f83451822ef8b5f173fa37
ahmedjoseph07 <ahmedjoseph11@gmail.com>
2024-07-15 14:52:04 +0600

    signup/login route implemented

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const path = require("path");
 const MONGO_URL = "mongodb://127.0.0.1:27017/haven";
 const methodOverride = require("method-override");
 const engine = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
-const listings = require("./routes/listing.js")
-const reviews = require("./routes/review.js")
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user")
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+
 
 // Database Connection
 main().then(() => {
@@ -31,7 +36,7 @@ app.use(methodOverride("_method"));
 app.engine("ejs", engine);
 app.use(express.static(path.join(__dirname, "public")));
 
-// express-session pkg & connect-flash pkg used
+//Express-session pkg & connect-flash pkg used
 const sessionOptions = {
     secret:"secretcode",
     resave:false,
@@ -50,15 +55,32 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 })
 
+// app.get("/demoUser", async(req,res)=>{
+//     let fakeUser = new User({
+//         email:"abc@gmail.com",
+//         username:"abc"
+//     })
+//     let registeredUser = await User.register(fakeUser,"passaword");
+//     res.send(registeredUser);
+// })
+
 // Routes (using express-router)
-app.use("/listings",listings)
-app.use("/listings/:id/reviews",reviews)
+app.use("/listings",listingRouter)
+app.use("/listings/:id/reviews",reviewRouter)
+app.use("/",userRouter)
 
 //Non existing routes error prevention
 app.all("*", (req, res, next) => {
---
+++ routes/user.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+router.get("/signup", (req, res) => {
+    res.render("users/signup.ejs")
+})
+
+router.post("/signup", wrapAsync(async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({ email, username });
+        const registeredUser = await User.register(newUser, password);
+        console.log(registeredUser);
+        req.flash("success", "Registered Successfully");
+        res.redirect("/listings");
+    } catch (err) {
+        req.flash("error", err.message);
+        res.redirect("/signup");
+    }
+}))
+
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs");
+})
+
+router.post("/login", passport.authenticate("local", { failureRedirect: '/login', failureFlash: true }), async(req, res) => {
+    req.flash("success","Welcome to Haven!");
+    res.redirect("/listings")
+})
+
+module.exports = router;
