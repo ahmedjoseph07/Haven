6a45d8120156a2d988eeecc662c4dc4f9ce09373
ahmedjoseph07 <ahmedjoseph11@gmail.com>
2024-08-23 22:38:08 +0600

    minor bug fixes

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
+const {saveRedirectUrl} = require("../middleware.js");
+
+
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs")
 })
@@ -12,7 +15,7 @@ router.post("/signup", wrapAsync(async (req, res) => {
         let { username, email, password } = req.body;
         const newUser = new User({ email, username });
         const registeredUser = await User.register(newUser, password);
-        console.log(registeredUser);
+        console.log(registeredUser); 
         req.login(registeredUser,(err)=>{
             if(err){
                 return next(err);
@@ -30,9 +33,10 @@ router.get("/login", (req, res) => {
     res.render("users/login.ejs");
 })
 
-router.post("/login", passport.authenticate("local", { failureRedirect: '/login', failureFlash: true }), async(req, res) => {
+router.post("/login",saveRedirectUrl,passport.authenticate("local", { failureRedirect: '/login', failureFlash: true }), async(req, res) => {
     req.flash("success","Welcome to Haven!");
-    res.redirect("/listings")
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
 })
 
 router.get("/logout",(req,res,next)=>{
